b0d49c7793f7453d7a553cc4b32bb7844f683e77
Hashimoto <daniel.hashimoto@accenture.com>
2024-09-26 19:26:02 -0300

    feat: validaçao de numerico com range

---
+++ src/app/validators/number.validator.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function rangeValidator(min: number, max: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const value = control.value;
+
+    if (value === '' || value === null) {
+        return null;
+    }
+
+    const numericValue = Number(value);
+    const isNumber = !isNaN(numericValue);
+    const inRange = isNumber && numericValue >= min && numericValue <= max;
+
+    return inRange ? null : { rangeError: { min, max } };
+  };
+}
